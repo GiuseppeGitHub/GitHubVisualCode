0868671b86c4fd1c6955d948357ccabdfa95a86d
Jose Luis <joseluismg2003@gmail.com>
2022-02-20 20:23:29 +0100

    Menu finalitzat

--- programaPython.py
+++ programaPython.py
@@ -7,11 +7,11 @@ numAleatori = random.randint(1,10)
 
 def MenuOpcions():
     print('''
-PULSA 'a' PER ADIVINAR UN NÚMERO
-PULSA 'b' PER SABER SI UN ANY ÉS DE TRASPAS
-PULSA 'c' PER SABER SI UN NÚMERO ÉS PRIMER
-PULSA 'd' PER CALCULAR EL MCD ENTRE DOS NÚMEROS
-PULSA 'o' PER SORTIR
+ENTRA 'a' PER ADIVINAR UN NÚMERO
+ENTRA 'b' PER SABER SI UN ANY ÉS DE TRASPAS
+ENTRA 'c' PER SABER SI UN NÚMERO ÉS PRIMER
+ENTRA 'd' PER CALCULAR EL MCD ENTRE DOS NÚMEROS
+ENTRA 'o' PER SORTIR
     ''')
 
 MenuOpcions()
@@ -28,4 +28,21 @@ while(tecla!='o'):
             print('ÉS UN ANY DE TRASPÀS')
         else:
             print('NO ÉS UN ANY DE TRASPÀS')
-    
+    if (tecla=='c'):
+        i = 2
+        numero = input('ENTRA UN NÚMERO: ')
+        while numero % i != 0 and i<= numero /2:
+            i+=1
+        if (numero%i ==0):
+            print('NO ÉS UN NÚMERO PRIMER')
+        else:
+            print('ÉS UN NÚMERO PRIMER')
+    if (tecla=='d'):
+        primerNumero=input('ENTRA UN NÚMERO:')
+        segonNumero=input('ENTRA UN SEGON NÚMERO: ')
+        while segonNumero !=0:
+            aux=segonNumero
+            segonNumero=primerNumero%segonNumero
+            primerNumero=aux
+        print(F'EL MÀXIM COMÚ DIVISOR ÉS {primerNumero}')
+
